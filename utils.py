d4ad6210beadbd4bebf089f3a1f4b62857566af9
Néstor Ojeda González <nestorojedagonzalez@gmail.com>
2025-03-02 10:04:17 +0000

    Refactor transformer training notebook and utility functions to improve code organization; add compute_metrics function for evaluation

--- utils.py
+++ utils.py
@@ -1,3 +1,6 @@
+import numpy as np
+import evaluate
+
 def transform_labels(example):
     # Create a mapping dictionary for sentiment labels
     sentiment_map = {
@@ -10,3 +13,14 @@ def transform_labels(example):
     # This handles both cases in one step and makes the code more maintainable
     return {'labels': sentiment_map.get(example['Sentiment'].lower(), 0)}
 
+def compute_metrics(eval_pred):
+    # load the metrics to use
+    load_accuracy = evaluate.load("accuracy")
+    load_f1 = evaluate.load("f1")
+
+    logits, labels = eval_pred
+    predictions = np.argmax(logits, axis=-1)
+    # calculate the mertic using the predicted and true value 
+    accuracy = load_accuracy.compute(predictions=predictions, references=labels)
+    f1 = load_f1.compute(predictions=predictions, references=labels, average="weighted")
+    return {"accuracy": accuracy, "f1score": f1}
